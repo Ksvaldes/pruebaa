96f3350952f1090b50dd9471658eb8d33b026eb2
Ksvaldes <ksvaldes@sscl.edu.gt>
2022-09-15 15:50:45 -0600

    Se agrega base de trabajo para experimentar

---
+++ Js/componentes/banner.js
@@ -0,0 +1,16 @@
+/**ESTRUCTURA DE HOME* */
+
+export let back = document.createElement("div");
+back.innerHTML = 
+`
+<section class="vector" >
+    <div id="Menu-interno"></div>
+    <h1 class="titulo1"> Aprende a <br> seguir un <br> camino sano </h1>
+    <img src="./img/Cocina 1.png" alt="" class="cocina">
+</section>
+`;
+
+export function Crearfondo(){
+    let div_sect = document.querySelector("#sect");
+    div_sect.appendChild(back);
+}
---
+++ Js/main.js
@@ -0,0 +1,15 @@
+/**CONEXIONDE HEADER YASMIN  */
+import {Crearheader} from "./componentes/nav.js";
+Crearheader();
+
+/****CONECCIÓN DE LA HOME** */
+import { Crearfondo } from "./componentes/banner.js";
+Crearfondo();
+
+/*MUESTRA PARTE DE KAREN VALDÉS*/
+import {AñadirInfo} from "./componentes/areaInformativa.js";
+AñadirInfo();
+
+/*MUESTRA PARTE DE BRYAN HERNADEZ*/
+import {CrearFooter} from "./componentes/footer.js";
+CrearFooter();
